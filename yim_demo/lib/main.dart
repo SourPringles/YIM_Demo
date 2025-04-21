b2bfe560719874a568d46c7754413d4e5554fd41
KJM <chance0425@naver.com>
2025-04-21 13:43:58 +0900

    Fork from debug application

--- yim_demo/lib/main.dart
+++ yim_demo/lib/main.dart
@@ -1,122 +1,351 @@
 import 'package:flutter/material.dart';
+import 'service/backend_service.dart';
+import 'settings_dialog.dart';
+import 'service/location_service.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MainApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MainPage(),
+      builder: (context, child) {
+        return Center(
+          child: ConstrainedBox(
+            constraints: const BoxConstraints(
+              maxWidth: 600, // 가로 600
+              maxHeight: 1200, // 세로 1200
+            ),
+            child: ClipRect(child: child),
+          ),
+        );
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MainPage> createState() => _MainPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MainPageState extends State<MainPage> {
+  final BackendService _backendService = BackendService();
+  List<Map<String, String>> _items = [];
+  bool _isFirstLoad = true;
+  //String _connectionStatus = ""; // 연결 상태 메시지
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _showSettingsDialog(
+        onClose: () {
+          _fetchInventory();
+        },
+      );
+    });
+  }
+
+  Future<void> _fetchInventory() async {
+    final items = await _backendService.fetchInventory();
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _items = items;
+      _isFirstLoad = false;
+    });
+  }
+
+  void _showSettingsDialog({VoidCallback? onClose}) {
+    showDialog(
+      context: context,
+      barrierDismissible: false, // 주변 여백 클릭으로 닫히지 않도록 설정
+      builder: (BuildContext context) {
+        return SettingsDialog(backendService: _backendService);
+      },
+    ).then((_) {
+      if (onClose != null) onClose();
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('MainPage'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.refresh),
+            onPressed: _fetchInventory,
+          ),
+          IconButton(
+            icon: const Icon(Icons.settings),
+            onPressed: () => _showSettingsDialog(),
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      body: Column(
+        children: [
+          Expanded(
+            child:
+                _isFirstLoad
+                    ? const Center(child: CircularProgressIndicator())
+                    : (_items.isEmpty
+                        ? const Center(child: Text('No items found'))
+                        : ListView.builder(
+                          itemCount: _items.length,
+                          itemBuilder: (context, index) {
+                            final item = _items[index];
+                            return ListTile(
+                              title: Text(item["nickname"] ?? ""),
+                              subtitle: Text(
+                                "UID: ${item["uuid"] ?? ""}\n"
+                                "Timestamp: ${item["timestamp"] ?? ""}\n"
+                                "Position: (${item["x"]}, ${item["y"]})",
+                              ),
+                              leading: const Icon(Icons.kitchen),
+                              trailing: const Icon(Icons.arrow_forward),
+                              onTap: () async {
+                                await Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder:
+                                        (context) => LocationPage(
+                                          backendService: _backendService,
+                                          highlightedItem: item,
+                                        ),
+                                  ),
+                                );
+                                _fetchInventory(); // 복귀 후 새로고침
+                              },
+                            );
+                          },
+                        )),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                onPressed: () async {
+                  await Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder:
+                          (context) =>
+                              LocationPage(backendService: _backendService),
+                    ),
+                  );
+                  _fetchInventory(); // 복귀 후 새로고침
+                },
+                child: const Text('물건 위치 보기'),
+              ),
+              ElevatedButton(
+                onPressed: _uploadImage,
+                child: const Text('이미지 업로드'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _uploadImage() async {
+    final result = await _backendService.uploadImage(context);
+    if (!mounted) return; // 위젯이 마운트되어 있는지 확인
+    if (result) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(const SnackBar(content: Text('이미지 업로드 성공')));
+    } else {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(const SnackBar(content: Text('이미지 업로드 실패')));
+    }
+
+    await _fetchInventory();
+  }
+}
+
+class LocationPage extends StatefulWidget {
+  final BackendService backendService;
+  final Map<String, String>? highlightedItem;
+
+  const LocationPage({
+    super.key,
+    required this.backendService,
+    this.highlightedItem,
+  });
+
+  @override
+  State<LocationPage> createState() => _LocationPageState();
+}
+
+class _LocationPageState extends State<LocationPage> {
+  late final LocationService _locationService;
+  List<Map<String, String>> _items = [];
+  bool _isLoading = true;
+  Map<String, String>? _highlightedItem; // 현재 강조된 물건
+
+  @override
+  void initState() {
+    super.initState();
+    _locationService = LocationService(widget.backendService);
+    _highlightedItem = widget.highlightedItem; // 초기 강조된 물건 설정
+    _fetchLocations();
+  }
+
+  Future<void> _fetchLocations() async {
+    final items = await _locationService.fetchLocations();
+    setState(() {
+      _items = items;
+      _isLoading = false;
+    });
+  }
+
+  void _clearHighlight() {
+    setState(() {
+      _highlightedItem = null; // 강조된 물건 초기화
+    });
+  }
+
+  Future<void> _updateNickname(String qrCode, String newNickname) async {
+    await widget.backendService.updateNickname(qrCode, newNickname);
+    await _fetchLocations();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('물건 위치 보기'),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.refresh),
+            onPressed: _fetchLocations,
+          ),
+        ],
+      ),
+      body:
+          _isLoading
+              ? const Center(child: CircularProgressIndicator())
+              : Stack(
+                children:
+                    _items.map((item) {
+                      final double x = double.tryParse(item["x"] ?? "0") ?? 0;
+                      final double y = double.tryParse(item["y"] ?? "0") ?? 0;
+                      final bool isHighlighted =
+                          _highlightedItem != null &&
+                          _highlightedItem!["uuid"] == item["uuid"];
+                      return Positioned(
+                        left: x,
+                        top: y,
+                        child: GestureDetector(
+                          onTap: () {
+                            _showItemDetailsDialog(item);
+                            _clearHighlight(); // 강조 초기화
+                          },
+                          child: Container(
+                            width: 90, // 가로 90
+                            height: 30, // 세로 30
+                            decoration: BoxDecoration(
+                              color: isHighlighted ? Colors.red : Colors.blue,
+                              shape: BoxShape.rectangle, // 사각형 형태
+                            ),
+                            child: Center(
+                              child: Text(
+                                item["nickname"] ?? "",
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 12, // 텍스트 크기 조정
+                                ),
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+                    }).toList(),
+              ),
+    );
+  }
+
+  void _showItemDetailsDialog(Map<String, String> item) {
+    final TextEditingController nicknameController = TextEditingController(
+      text: item["nickname"],
+    );
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(item["nickname"] ?? "Unknown"),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Table(
+                columnWidths: const {
+                  0: IntrinsicColumnWidth(),
+                  1: FlexColumnWidth(),
+                },
+                children: [
+                  TableRow(
+                    children: [
+                      const Text('UID:'),
+                      Text(item["uuid"] ?? "Unknown"),
+                    ],
+                  ),
+                  TableRow(
+                    children: [
+                      const Text('Timestamp:'),
+                      Text(item["timestamp"] ?? "Unknown"),
+                    ],
+                  ),
+                  TableRow(
+                    children: [
+                      const Text('Position:'),
+                      Text("(${item["x"]}, ${item["y"]})"),
+                    ],
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              TextField(
+                controller: nicknameController,
+                decoration: const InputDecoration(labelText: 'Edit Nickname'),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('닫기'),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                final newNickname = nicknameController.text;
+                await _updateNickname(item["uuid"] ?? "", newNickname);
+                Navigator.pop(context);
+              },
+              child: const Text('수정'),
             ),
           ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        );
+      },
     );
   }
 }
