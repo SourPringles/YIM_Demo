5f69c418bcd61d59e4c3d861b2b314b9363b2f5d
KJM <chance0425@naver.com>
2025-05-05 21:19:47 +0900

    .

--- yim_demo/lib/main.dart
+++ yim_demo/lib/main.dart
@@ -15,7 +15,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: const MainPage(),
+      home: const VisualViewPage(),
       builder: (context, child) {
         return Center(
           child: ConstrainedBox(
--- yim_demo/lib/views/visualviewpage.dart
+++ yim_demo/lib/views/visualviewpage.dart
@@ -2,45 +2,25 @@ import 'package:flutter/material.dart';
 
 import '../service/visualviewpage_service.dart';
 
-class MainPage extends StatefulWidget {
-  const MainPage({super.key});
+class VisualViewPage extends StatefulWidget {
+  const VisualViewPage({super.key});
 
   @override
-  State<MainPage> createState() => _MainPageState();
+  State<VisualViewPage> createState() => _MainPageState();
 }
 
-class _MainPageState extends State<MainPage> {
+class _MainPageState extends State<VisualViewPage> {
   final MainPageService _mainPageService = MainPageService();
-  List<Map<String, String>> _items = [];
 
   @override
   void initState() {
     super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      _showSettingsDialog(
-        onClose: () {
-          _fetchStorage();
-        },
-      );
-    });
   }
 
   Future<void> _fetchStorage() async {
     final items = await _mainPageService.fetchStorage();
     setState(() {
       _items = items;
-    });
-  }
-
-  void _showSettingsDialog({VoidCallback? onClose}) {
-    showDialog(
-      context: context,
-      barrierDismissible: false, // 주변 여백 클릭으로 닫히지 않도록 설정
-      builder: (BuildContext context) {
-        return SettingsDialog(backendService: _backendService);
-      },
-    ).then((_) {
-      if (onClose != null) onClose();
     });
   }
 
@@ -52,10 +32,7 @@ class _MainPageState extends State<MainPage> {
         actions: [
           IconButton(icon: const Icon(Icons.refresh), onPressed: _fetchStorage),
 
-          IconButton(
-            icon: const Icon(Icons.settings),
-            onPressed: () => _showSettingsDialog(),
-          ),
+          IconButton(icon: const Icon(Icons.settings), onPressed: () => asdf()),
         ],
       ),
       body: Column(
@@ -86,21 +63,5 @@ class _MainPageState extends State<MainPage> {
         ],
       ),
     );
-  }
-
-  Future<void> _uploadImage() async {
-    final result = await _backendService.uploadImage(context);
-    if (!mounted) return; // 위젯이 마운트되어 있는지 확인
-    if (result) {
-      ScaffoldMessenger.of(
-        context,
-      ).showSnackBar(const SnackBar(content: Text('이미지 업로드 성공')));
-    } else {
-      ScaffoldMessenger.of(
-        context,
-      ).showSnackBar(const SnackBar(content: Text('이미지 업로드 실패')));
-    }
-
-    await _fetchInventory();
   }
 }
