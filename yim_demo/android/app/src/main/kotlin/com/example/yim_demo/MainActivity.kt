a50a563c9c17cadf7f9599bf381b10e6653dad0f
KJM <chance0425@naver.com>
2025-05-30 13:20:00 +0900

    fix ctd error

--- yim_demo/android/app/src/main/kotlin/com/example/yim_demo/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.yim_demo
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ yim_demo/android/app/src/main/kotlin/com/sourpringles/yim_demo/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.sourpringles.yim_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
